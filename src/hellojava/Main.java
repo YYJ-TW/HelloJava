9c4189f37343221cf573930fe754278763a6e7ef
YYJ <68520446+YYJ-TW@users.noreply.github.com>
2022-04-23 19:30:40 +0800

    Update notes

--- src/hellojava/Main.java
+++ src/hellojava/Main.java
@@ -1,9 +1,9 @@
-package hellojava;
+package hellojava; // package建議為小寫
 
 public class Main {
     public static void main(String[] args) { // 程式的入口
 
-        System.out.println("Hello Java!");
+        System.out.println("Hello Java!"); // 列印出"Hello Java"字串｜print不換行列印｜println換行列印
 
     }
 }
